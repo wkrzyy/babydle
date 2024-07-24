169fd66b597b1536e00f03770aed862bfaebac6f
jellis-pvcc <jme20581@email.vccs.edu>
2024-07-24 15:19:36 -0400

    Update WordleEngine.py
    Removed Function

--- WordleEngine.py
+++ WordleEngine.py
@@ -2,37 +2,28 @@
 import csv
 import random
 
-
+def main(): 
+    # open the file in read mode - That's what the 'r' means
+    filename = open('baby-names-4.csv', 'r')
  
-# open the file in read mode - That's what the 'r' means
-filename = open('baby-names-4.csv', 'r')
+    # creating dictreader object
+    file = csv.DictReader(filename)
  
-# creating dictreader object
-file = csv.DictReader(filename)
- 
-# creating empty lists
-names = []
+    # creating empty lists
+    names = []
 
-# Assuming the column name is "names"
-for row in file:
-    names.append(row['name'])
+    # Assuming the column name is "names"
+    for row in file:
+        names.append(row['name'])
+    #print("DEBUG" + len(names))
+    #print("DEBUG: " + 'Names:', names)
 
-
-# printing lists
-print(len(names))
-print('Names:', names)
-
-def setUpGame():
     answer = names[random.randint(0, len(names))]
     answer = answer.lower()
-    #print(answer)
-
-    numberOfGuesses = 0
-    gameWon = False
-
-
-def playGame():
+    
     #Game Loop/Update Loop
+    numberOfGuesses = 0
+    print("DEBUG: " + answer)
     while(1):
         #guess: Stores the users Guess
         guess = input("Make a guess: ")
@@ -43,34 +34,37 @@ def playGame():
         # "-" = the letter is in a misplaced location
         # "?" = the letter could be misplaced or in the wrong location
         temp = ""
+        temp2 = answer
     
         if(len(guess) != len(answer)):
             print("Only use " + str(len(answer)) + " characters.")
         elif(guess == answer):
             print("You are correct! The name was " + answer)
-            print("It took you " + str(numberOfGuesses) + " guesses")
+            print("It took you " + str(numberOfGuesses + 1) + " guesses")
             print("")
             print("")
             print("A new Game Has Started")
         
             answer = names[random.randint(0, len(names))]
             answer = answer.lower()
-            #print(answer)
+            print("DEBUG: " + answer)
             numberOfGuesses = 0
-            gameWon = False
         else:
-            for i in range(0, len(guess)):
-                if(guess[i] == answer[i]):
+            for index in range(0, len(guess)):
+                if(guess[index] == temp2[index]):
                     temp += "✓"
-                elif(guess[i] != answer[i] and guess[i] not in answer):
-                    temp += "X"
+                    temp2 = temp2.replace(guess[index], "^",1)
                 else:
-                    temp += "-"
+                    temp += "?"
+            for index in range(0, len(guess)):
+                if(guess[index] != temp2[index] and temp[index] == "?" and guess[index] not in temp2):
+                    temp = temp[:index] + "X" + temp[index + 1:]
+                elif(temp[index] == "?"):
+                    temp = temp[:index] + "-" + temp[index + 1:]
+                    temp2 = temp2.replace(guess[index], "^",1)
+            numberOfGuesses += 1
         print(temp)
-        numberOfGuesses += 1
-
-def main():
-    setUpGame()
-    playGame()
+        #print(temp2)
+        
 
 main()
